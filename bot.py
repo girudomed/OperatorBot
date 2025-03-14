c9930bac1303e2d9e0b5e98f9dc8e7d7976af995
girudomed <girudomed.kazan@gmail.com>
2025-03-14 15:56:26 +0200

    убрал возврат ошибки пользователю

--- bot.py
+++ bot.py
@@ -3843,7 +3843,7 @@ class TelegramBot:
         if not self.scheduler.running:
             self.scheduler.start()
         self.scheduler.add_job(
-            self.send_daily_reports, "cron", hour=13, minute=51
+            self.send_daily_reports, "cron", hour=13, minute=57
         )  # поставить 6 утра, на проде будет не локальное мое время
         logger.info("Ежедневная задача для отправки отчетов добавлена в планировщик.")
         # Запуск воркеров
